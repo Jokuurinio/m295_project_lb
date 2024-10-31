c161c56d27197ef115f947433de247b256ec909c
Jokuurinio <fabian.wehrle63@gmail.com>
2024-10-31 16:06:15 +0100

    initial commit

---
+++ lb_project/src/main/java/ch/wiss/m295/lb_project/LbProjectApplication.java
@@ -0,0 +1,13 @@
+package ch.wiss.m295.lb_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbProjectApplication.class, args);
+	}
+
+}
---
+++ lb_project/src/test/java/ch/wiss/m295/lb_project/LbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.wiss.m295.lb_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
